556409e9b81ef77b2355469eb527bf3f1fb6b363
Peter Torr Smith <peter@ptorrsmith.com>
2019-01-13 18:31:43 +1300

    Add custom route for portfolio_item_show from default of portfolio_items

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-  resources :portfolio_items
+  resources :portfolio_items, except: [:show]
+  get 'portfolio_item/:id', to: 'portfolio_items#show', as: 'portfolio_item_show'
 
   # get 'pages/home'
   get 'about-me', to: 'pages#about'

b6a1469ca8c659c7a25d177b4ae249fa770b4e0d
Peter Torr Smith <peter@ptorrsmith.com>
2019-01-17 08:39:39 +1300

    Concerns for placeholder image

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,4 +1,5 @@
 class PortfolioItem < ApplicationRecord
+    include PlaceholderImage
     validates_presence_of :title, :body, :main_image, :thumb_image
 
     # define scope option 1
@@ -12,8 +13,11 @@ class PortfolioItem < ApplicationRecord
     after_initialize :set_defaults
 
     def set_defaults
-        self.main_image ||= 'http://placehold.it/600x400'
-        self.thumb_image ||= 'http://placehold.it/350x200'
+        # self.main_image ||= 'http://placehold.it/600x400'
+        # self.thumb_image ||= 'http://placehold.it/350x200'
+
+        self.main_image ||= PlaceholderImage.image_generator(height: '600', width: '450')
+        self.thumb_image ||= PlaceholderImage.image_generator(height: 350, width: 250)
     end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
+    include PlaceholderImage
     validates_presence_of :title, :percent_utilized
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.badge ||= PlaceholderImage.image_generator(height: '200', width: '200')
 end

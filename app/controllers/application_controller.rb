4e492f3f1e3ccc4ac3a28827ce116f06abd1106f
Peter Torr Smith <peter@ptorrsmith.com>
2019-02-03 12:39:40 +1300

    moved params whitelist for devise into concern module with before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    # noticed protect_from_forgery appears to be in Base, so no longer declared here
+
+    # Moved to application concern module devise_whitelist.rb
+    # before_action :configure_permitted_parameters, if: :devise_controller?
 
 
-    # whitelisting allowed forme fields in posts, puts, patches etc
-        def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-    end
+    # # whitelisting allowed form fields in posts, puts, patches etc
+    #     def configure_permitted_parameters
+    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    #     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    # end
+
+    include DeviseWhitelist
 
 
 end

0287457478b3f4fb435ebde1f17af5c87b21ac65
Peter Torr Smith <peter@ptorrsmith.com>
2019-02-03 13:09:43 +1300

    Virtual attribtue for first and last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :name
+
+  def first_names
+    # if self.name.split.length > 2
+    #   self.name.split.first # split into array by space
+    # else
+      self.name.split.first # split into array by space
+    # end
+  end
+
+  def last_name
+    self.name.split.last
+
+  end
 end

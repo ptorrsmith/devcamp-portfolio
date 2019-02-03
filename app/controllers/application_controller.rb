c760f76fa9204484a7c9da69f0492a59ed3cdc41
Peter Torr Smith <peter@ptorrsmith.com>
2019-02-03 16:30:38 +1300

    Use null object pattern to return faux guest user if not real user, and test in app layout for class of User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,27 +5,37 @@ class ApplicationController < ActionController::Base
     # so modules have single responsibility
     include DeviseWhitelist
     include SetSource
-    
+    include CurrentUserConcern
+
+
+       
     # noticed protect_from_forgery appears to be in Base, so no longer declared here
 
-    # Moved to application concern module devise_whitelist.rb
+    # MOVED TO CONCERN MODULE
     # before_action :configure_permitted_parameters, if: :devise_controller?
-
-
     # # whitelisting allowed form fields in posts, puts, patches etc
     #     def configure_permitted_parameters
     #     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     #     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     # end
 
-    # moved to application concern moule SetSource
+    # MOVED TO CONCERN MODULE
     # # do before every 
     # before_action :set_source
-
     
     # # capture source if linked from other site
     # def set_source
     #     session[:source] = params[:q] if params[:q]    
+    # end
+
+    
+    
+    # MOVED TO CONCERN MODULE
+    # override / overload the current user from devise
+    # so never return null/nil if no logged in user (e.g guest / anon)
+    
+    # def current_user
+    #     super || OpenStruct.new(name: "Guest visitor", first_names: "Guesty", last_name: "visitori", email: "x@y.z")
     # end
 
 

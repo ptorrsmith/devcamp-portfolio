1250421b5046aac02ef585f1f1869ec685fb82ff
Peter Torr Smith <peter@ptorrsmith.com>
2019-02-03 19:30:35 +1300

    View helper for auth links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+    def sample_helper
+        "<p>My Helper</p>".html_safe # needed to parse HTML, else will just be text with tages
+        # don't use .html_safe with user input
+
+    end
+
+    def login_helper
+        if current_user.is_a?(User)
+        link_to "Log out", destroy_user_session_path, method: :delete
+      else
+        (link_to "Register", new_user_registration_path) + '<br />'.html_safe + (link_to "Log in", new_user_session_path)
+      end
+    end
 end

35eef9bb91845817ff3a3a34f21edcace7d28e92
Peter Torr Smith <peter@ptorrsmith.com>
2019-02-25 15:50:51 +1300

    Refactored portfolio_item_img helper

--- app/helpers/portfolio_items_helper.rb
+++ app/helpers/portfolio_items_helper.rb
@@ -7,21 +7,14 @@ module PortfolioItemsHelper
 
     def portfolio_item_img item, type
         # byebug
-        # expects a PortfolioItemUploader object
+        # expects a PortfolioItemUploader object (carrierwave)
         # this is a bit wierd, as have to access thumb image up to model, down into value!!?!?!?! 
         # byebug
         if type == 'thumb'
-            if item.thumb_image.to_s.length > 0
-                item.thumb_image.to_s
-            else 
-                image_generator(height: '350', width: '200')
-            end
+            # byebug
+            item.thumb_image.url || image_generator(height: '350', width: '200')
         else # 'main'
-            if item.main_image.to_s.length > 0
-                item.main_image.to_s
-            else
-                image_generator(height: '600', width: '400')
-            end
+            item.main_image.url || image_generator(height: '600', width: '400')
         end
         # byebug
     end

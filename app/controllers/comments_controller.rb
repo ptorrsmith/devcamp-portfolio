3d2807ec6c66da02a85d0273d5b12b863ae49309
Peter Torr Smith <peter@ptorrsmith.com>
2019-03-04 13:27:31 +1300

    Built initial comments form on blog show, and partial for showing existing blogs. Controller action for blog show also to create empty comment for comment form on the blog

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+    # no views for comments, as these are parts of other views (blog, user)
+    def create
+        # automatically associates comment with user (and blog supplied in params?)
+        @comment = current_user.comments.build(comment_params)
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
 end

038cccffbbc626013e5053241c16eba10dc80b6d
Peter <peter@ptorrsmith.com>
2020-09-13 14:53:50 +1200

    V 2.1.04 sort_order default 99

--- db/migrate/20200913021531_add_order_to_skills.rb
+++ db/migrate/20200913021531_add_order_to_skills.rb
@@ -1,5 +1,5 @@
 class AddOrderToSkills < ActiveRecord::Migration[6.0]
   def change
-    add_column :skills, :sort_order, :integer, :default => 1
+    add_column :skills, :sort_order, :integer, :default => 99
   end
 end

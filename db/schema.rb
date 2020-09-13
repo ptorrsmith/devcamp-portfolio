8cf7f30139c9b7b8be7fa88e2d3007be98f8bd14
Peter <peter@ptorrsmith.com>
2020-09-13 22:20:16 +1200

    V 2.1.15 remove hightlights column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_091319) do
+ActiveRecord::Schema.define(version: 2020_09_13_100757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2020_09_13_091319) do
     t.date "to"
     t.text "roles"
     t.text "description"
-    t.text "hightlights"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "current"

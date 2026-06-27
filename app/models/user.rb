03f25ff3478216e8129767475a51a32506b05fb1
Peter Torr Smith <peter@ptorrsmith.com>
2026-06-27 14:41:12 +1200

    Version 2.7.17 remove signup from Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:site_admin, :editor], multiple: false)                                      ##
-  ############################################################################################ 
- 
+  petergate(roles: %i[site_admin editor], multiple: false) ##
+  ############################################################################################
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :name
@@ -22,12 +24,11 @@ class User < ApplicationRecord
     # if self.name.split.length > 2
     #   self.name.split.first # split into array by space
     # else
-      self.name.split.first # split into array by space
+    name.split.first # split into array by space
     # end
   end
 
   def last_name
-    self.name.split.last
-
+    name.split.last
   end
 end
